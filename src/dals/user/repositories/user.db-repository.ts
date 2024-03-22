10fe4ae02dd07da1f896593677ed8ab37c0c6756
Samuel Jurado Quintana <samueljurado12@gmail.com>
2024-03-22 18:30:47 +0100

    Added user.db-repository functionality

--- src/dals/user/repositories/user.db-repository.ts
+++ src/dals/user/repositories/user.db-repository.ts
@@ -1,8 +1,19 @@
+import { hashPassword } from "common/helpers";
+import { getUserContext } from "../user.context";
 import { User } from "../user.model";
 import { UserRepository } from "./user.repository";
 
 export const dbUserRepository: UserRepository = {
     getUserByEmailAndPassword: async (email: string, password: string): Promise<User> => {
-        throw new Error("Function not implemented.");
+        const user = await getUserContext().findOne({
+            email,
+        });
+
+        const hashedPassword = await hashPassword(password, user?.salt);
+        return user?.password === hashedPassword ? ({
+            _id: user._id,
+            email: user.email,
+            role: user.role
+        } as User) : null
     }
 }
